6436ce5756ba59fea0adfcebf87d08255c84d272
Nickolaus Gotsiridze <ngotsiridze@accumtech.com>
2024-07-31 23:06:58 -0400

    Updated User, Post, and Comment models and seeded data

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,35 +1,30 @@
 from django.db import models
 
 class User (models.Model):
-    #is this necessary?? If so, how to make they're unique numbers?
-    userId = models.IntegerField()
-
     username = models.CharField(max_length=500)
     password = models.CharField(max_length=500)
     display_name = models.CharField(max_length=500)
 
-class Post(models.Model): 
-    #is this necessary?? If so, how to make they're unique numbers?
-    postId = models.IntegerField()
-    #necessary? 
-    userId = models.IntegerField()
+    def __str__(self):
+        return self.username
 
+class Post(models.Model): 
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField(max_length=200000)
     published_date = models.DateTimeField(auto_now_add=True)
     likes = models.IntegerField(default=0)
-    commentCount = models.IntegerField(default=0)
-    
+    comment_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
     
 class Comment(models.Model):
-    #is this necessary?? If so, how to make they're unique numbers?
-    commentId = models.IntegerField()
-    #necessary? 
-    postId = models.IntegerField()
-    
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return self.content 
 
 
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Post
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+def home(request):
+    posts = [
+        Post(title="Post 1", content="content of post 1"),
+        Post(title="Post 2", content="content of post 2"),
+        Post(title="Post 3", content="content of post 3")
+    ]
+    
+    return render(request, 'home.html', {'posts' : posts})
