ca47a1e0766583520c6acd3aecd7cbf72155d4bf
Nickolaus Gotsiridze <ngotsiridze@accumtech.com>
2024-09-09 23:28:07 -0400

    Created travel, coding, and health urls

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("travel", views.travel, name="travel"),
+    path("coding", views.coding, name="coding"),
+    path("health", views.health, name="health"),
     path("post/<int:post_id>/", views.post, name="post_content")
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -7,6 +7,22 @@ def home(request):
     context = {"posts": posts}
     return render (request, "blogapp/home.html", context)
 
+# TODO: consolidate travel, coding, health (views are too similar)
+def travel(request):
+    posts = Post.objects.filter(category='travel').order_by("published_date")
+    context = {"posts": posts}
+    return render (request, "blogapp/travel.html", context)
+
+def coding(request):
+    posts = Post.objects.filter(category='coding').order_by("published_date")
+    context = {"posts": posts}
+    return render (request, "blogapp/coding.html", context)
+
+def health(request):
+    posts = Post.objects.filter(category='health').order_by("published_date")
+    context = {"posts": posts}
+    return render (request, "blogapp/health.html", context)
+
 def post(request, post_id): 
     post = get_object_or_404(Post, pk=post_id)
     context = {"post": post}
