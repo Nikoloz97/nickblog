b941b76efcfdf40b5b584d4e8deb6b6f1a621e5a
Nikoloz97 <nick.gotsy@gmail.com>
2025-04-23 11:19:06 -0400

    Added static directory for css overrides; changed page theme to lux

--- nickblog/settings.py
+++ nickblog/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'blogapp', 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
